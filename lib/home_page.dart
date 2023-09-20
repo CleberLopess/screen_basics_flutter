342bfff45c09f35f6c07958ce8a8cf3489bed4bc
Cleber Lobster <cleberlopes777@gmail.com>
2023-09-20 09:54:50 -0300

    my first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = TextEditingController();
+  final list = <String>[];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      alignment: Alignment.topLeft,
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/img/background.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          color: Colors.black.withOpacity(0.4),
+        ),
+        SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FlutterLogo(
+                size: 150,
+                style: FlutterLogoStyle.stacked,
+                textColor: Colors.white,
+              ),
+              ElevatedButton(onPressed: () => {}, child: Text('Entrar no app'))
+            ],
+          ),
+        ),
+      ],
+    ));
+  }
+}
